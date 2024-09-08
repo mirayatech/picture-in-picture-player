73133041243f30b08faf9bc149b937c87aeefbd1
mirayatech <mirayaabrodi@gmail.com>
2024-09-08 14:09:07 +0200

    feat: create VideoDescription component

---
+++ src/components/VideoDescription.tsx
@@ -0,0 +1,15 @@
+export function VideoDescription() {
+  return (
+    <>
+      <h1 className="mb-4 text-3xl leading-tight text-slate-900">
+        This video highlights Nezuko surviving sunlight in Demon Slayer Season
+        3, Episode 11.
+      </h1>
+      <p className="mb-4 text-slate-600 opacity-80">
+        In this important moment, Nezuko surprises everyone by surviving
+        sunlight. This is a big change for her and the story, and fans will love
+        the emotion and great animation in this scene.
+      </p>
+    </>
+  );
+}
