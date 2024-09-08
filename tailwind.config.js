5db01cf212cfe2e658b42d43b305ff080133bce7
mirayatech <mirayaabrodi@gmail.com>
2024-09-08 14:05:08 +0200

    feat: add custom animations for videoInline and videoSticky

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        videoInline: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        videoSticky: {
+          from: {
+            transform: "translateX(50px)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateX(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        videoInline: "videoInline 300ms ease-in-out forwards",
+        videoSticky: "videoSticky 300ms ease-in-out forwards",
+      },
+    },
   },
   plugins: [],
-}
+};
